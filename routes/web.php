aa3ac5dea1899b34b74c9281a88bde43dca4fe54
Lexie <alexandriamogote1216@gmail.com>
2026-08-12 11:24:08 +0800

    Add basic GET string routes for hello and school

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route ::get('/hello', function() {
+    return 'Hello, Laravel routing!';
+});
+Route ::get('/school', function(){
+    return 'Welcome to our Laravel Class!';
+});
